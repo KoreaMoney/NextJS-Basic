33cab094ad4ba2d02d63341f78b8a5849132e0e4
KIMDOWON <dw.kim@theinfoin.com>
2023-11-29 11:30:55 +0900

    feat : SSG구축

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -10,3 +10,9 @@ const Pants = ({ params }: Iprops) => {
     return <div>상품항목 - {params.slug}</div>;
 };
 export default Pants;
+
+export function generateStaticParams() {
+    // 미리 만들어 놓고 싶은 정적페이지 구성하기
+    const staticProducts = ['pants, shirt'];
+    return staticProducts.map((item) => ({ slug: item }));
+}
