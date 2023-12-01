2a09fa1ad18b7661d7384bf3192dadb736bc3824
DowonKim <kimdowon1404@gmail.com>
2023-12-01 14:07:27 +0900

    feat : 서버 컴포넌트 내용 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
+import os from 'os';
 
 export default function Home() {
+    console.log('os', os.hostname());
     return (
         <>
             <h1>Space to record a day, a week, a month, and a year</h1>
